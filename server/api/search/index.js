a98a43dc2482605a70ee5c6569dd2bd816b8600c
monojw <97877334+monojw@users.noreply.github.com>
2022-06-30 20:52:46 +0900

    search API

---
+++ server/api/search/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getSearchResult } from './search.ctrl.js';
+
+const router = express.Router();
+
+router.get('/', getSearchResult);
+
+export default router;
