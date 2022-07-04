19d3ae6a73b342ff1d010d1b8a3553ec42aa5fba
milmilkim <soundkim27@gmail.com>
2022-07-04 22:46:07 +0900

    Update axios.js

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -24,19 +24,17 @@ axios.interceptors.response.use(
   async (error) => {
     const { response: errorResponse, config: originalRequest } = error;
 
-    console.log(originalRequest);
-
     if (errorResponse.status === 401) {
       const { data } = await Axios.get("/api/auth/refresh", {
         baseURL: process.env.REACT_APP_BASE_URL,
         params: {
           token: window.localStorage.getItem("refreshToken"),
         },
-      }); //리프레시 토큰을 요청하고 저장
+      }); //리프레시 토큰으로 새 액세스 토큰 요청
 
       const { accessToken: newAccessToken } = data;
-      window.localStorage.setItem("accessToken", newAccessToken);
-      originalRequest.headers["x-access-token"] = newAccessToken;
+      window.localStorage.setItem("accessToken", newAccessToken); //새 액세스 토큰을 로컬스토리지에 저장
+      originalRequest.headers["x-access-token"] = newAccessToken; //새 액세스 토큰을 헤더에 설정
       return Axios(originalRequest); //재요청
     }
     return Promise.reject(error);
