6e9906739c70c153a9e412ff3f3f2828a0d27c21
monojw <monojw.io@gmail.com>
2022-07-05 17:44:31 +0900

    SearchSlice 생성

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./slices/AuthSlice";
 import BookListSlice from "./slices/BookListSlice";
+import SearchSlice from "./slices/SearchSlice";
 
 const store = configureStore({
   reducer: {
     booklist: BookListSlice,
     auth: AuthSlice,
+    search: SearchSlice,
   },
 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
