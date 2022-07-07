66569567cea64113d08e9dc2983d81700ab0a038
monojw <monojw.io@gmail.com>
2022-07-07 17:05:43 +0900

    ReviewSlice 추가

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./slices/AuthSlice";
 import BookListSlice from "./slices/BookListSlice";
 import SearchSlice from "./slices/SearchSlice";
+import ReviewSlice from "./slices/ReviewSlice";
 
 const store = configureStore({
   reducer: {
     booklist: BookListSlice,
     auth: AuthSlice,
     search: SearchSlice,
+    review: ReviewSlice,
   },
 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
