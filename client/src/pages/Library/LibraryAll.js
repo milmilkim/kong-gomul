88b553a3da3d1ff9e15ade8ed185646a7aea704a
monojw <monojw.io@gmail.com>
2022-07-12 15:43:04 +0900

    내서재 페이지 수정

--- client/src/pages/Library/LibraryAll.js
+++ client/src/pages/Library/LibraryAll.js
@@ -1,11 +1,12 @@
 /**
  * 내 서재 페이지 (전체)
  */
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 /* Slice */
 import { getReviewList } from "../../slices/ReviewSlice";
+import { getMyProfile } from "../../slices/MemberSlice";
 
 /* Components */
 import BooksItem from "../../components/BooksItem";
@@ -15,15 +16,32 @@ import Spinner from "../../components/spinner";
 
 const LibraryAll = memo(() => {
   const dispatch = useDispatch();
-  const { data, loading } = useSelector((state) => state.review); // 검색결과, 로딩여부
+  const { loading } = useSelector((state) => state.review); // 로딩여부
+
+  const [memberId, setMemberId] = useState(); // 멤버 아이디
+  const [reviewList, setReviewList] = useState(); // 유저가 작성한 리뷰 목록들
 
   useEffect(() => {
-    dispatch(
-      getReviewList({
-        member_id: 18, // <--------
-      })
-    );
-  }, [dispatch]);
+    async function fetchData() {
+      const {
+        payload: {
+          data: { id },
+        },
+      } = await dispatch(getMyProfile()); // 멤버 아이디를 가져온다.
+      setMemberId(id);
+
+      const {
+        payload: { data: reviewData },
+      } = await dispatch(
+        getReviewList({
+          member_id: memberId,
+        })
+      ); // 가져온 멤버 아이디와 일치하는 회원이 작성한 리뷰 목록을 가져온다.
+      setReviewList(reviewData);
+    }
+
+    fetchData();
+  }, [dispatch, memberId]);
 
   return (
     <>
@@ -34,7 +52,7 @@ const LibraryAll = memo(() => {
       <LibraryNav />
 
       {/* Book List */}
-      <ItemList>{data && data.map((book, index) => <BooksItem book={book.book} key={index} />)}</ItemList>
+      <ItemList>{reviewList && reviewList.map((book, index) => <BooksItem book={book.book} key={index} />)}</ItemList>
     </>
   );
 });
