5d3346d6b49f3cb88dfa74bb40df85f8d20a0f79
milmilkim <soundkim27@gmail.com>
2022-07-18 02:17:44 +0900

    reivew에 미들웨어 추가

--- server/api/review/index.js
+++ server/api/review/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { getReviewList, getReview, addReview, patchReview } from './review.ctrl.js';
+import authMiddleware from '../../middlewares/auth.js';
 
 const router = express.Router();
 
 router.get('/', getReviewList);
 router.get('/:id', getReview);
+
+router.use('/', authMiddleware);
 router.post('/:book_id', addReview);
 router.patch('/:id', patchReview);
 
